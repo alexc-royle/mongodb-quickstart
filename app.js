3f3544845ef8767ce18519e68b0aaccac4621f7a
Alex Royle <alex.royle@mydomain.com>
2017-02-21 22:19:00 +0000

    Adding additional functions to app.js

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ MongoClient.connect(url, function(err, db) {
 	console.log("Connected successfully to server");
 
 	insertDocuments(db, function() {
-		findDocuments(db, function() {
-			findCertainDocuments(db, function() {
-				db.close();
+		indexCollection(db, function() {
+			findDocuments(db, function() {
+				findCertainDocuments(db, function() {
+					updateDocument(db, function() {
+						removeDocument(db, function() {
+							findDocuments(db, function() {
+								removeAllDocuments(db, function() {
+									db.close();
+								});
+							})
+						});
+					});
+				});
 			});
 		});
 	});
-	
 });
 
 var insertDocuments = function(db, callback) {
@@ -55,4 +64,50 @@ var findCertainDocuments = function(db, callback) {
 		console.log(docs);
 		callback(docs);
 	})
+}
+
+var updateDocument = function(db, callback) {
+	// Get the documents collection
+	var collection = db.collection('documents');
+	// Update document where a is 2, set b equal to 1
+	collection.updateOne(
+		{ a : 2 }, 
+		{ $set: { b : 1 } }, 
+		function(err, result) {
+			assert.equal(err, null);
+			assert.equal(1, result.result.n);
+			console.log('Updated the document with the field a equal to 2');
+			callback(result);
+		}
+	);
+}
+
+var removeAllDocuments = function(db, callback) {
+	var collection = db.collection('documents');
+	collection.remove({});
+	console.log('All documents have been removed');
+	callback(true);
+}
+
+var removeDocument = function(db, callback) {
+	// Get the documents collection
+	var collection = db.collection('documents');
+	//Delete document where a is 3
+	collection.deleteOne({ a : 3 }, function(err, result) {
+		assert.equal(err, null);
+		assert.equal(1, result.result.n);
+		console.log('Removed the document with the field a equal to 3');
+		callback(result);
+	})
+}
+
+var indexCollection = function(db, callback) {
+	db.collection('documents').createIndex(
+		{ 'a' : 1 },
+		null,
+		function(err, results) {
+			console.log('indexCollection results:');
+			console.log(results);
+			callback();
+		})
 }
